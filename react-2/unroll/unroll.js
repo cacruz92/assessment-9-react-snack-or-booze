ba60ef39d44f01d91e1e0d418f58b2f29e48dc2e
Christian Cruz <christian.cruz92@hotmail.com>
2024-07-07 19:31:40 -0400

    updated the unroll function to make it work correctly and for it to repeat until all the numbers have been copied to the new array no matter the size of the original array.

--- react-2/unroll/unroll.js
+++ react-2/unroll/unroll.js
@@ -1,5 +1,65 @@
 function unroll(squareArray) {
+    let newArray = [];
 
+    if(squareArray.length === 0) return newArray;
+
+    let top = 0;
+    let bottom = squareArray.length-1;
+    let left = 0;
+    let right = squareArray[0].length-1;
+
+    while(top <= bottom && left <= right){
+
+        //going from top-left to top-right
+        for(let i = left; i <= right; i++){
+            newArray.push(squareArray[top][i])
+        }
+        // move down one row, while the index is still in the last spot in the array
+        top++;
+
+        // keep moving downward along the right side of the square
+        for(let i = top; i <= bottom; i++){
+            newArray.push(squareArray[i][right])
+        }
+        // move to the left, while staying in the bottom array
+        right--;
+
+        // move to the left
+        for(let i = right; i >= left; i--){
+            newArray.push(squareArray[bottom][i])
+        }
+        // move up
+        bottom--
+
+        if (left <= right){
+            //move upward
+            for(let i = bottom; i >= top; i--){
+                newArray.push(squareArray[i][left])
+            }
+            //move to the right
+            left++
+        }
+    }
+
+
+
+
+    console.log(newArray)
+    return newArray
 }
 
+const array = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+unroll(array);
+        
+    
+
+
 module.exports = unroll;
+
+
+
